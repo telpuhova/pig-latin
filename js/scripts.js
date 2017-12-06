a18874502c7d2db80b7e75675e4bedfd5701feef
Kate Trahan and Natalia Telpukhova <kate@>
2017-12-06 11:54:27 -0800

    add spec: sentence with multiple words

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,27 @@
 var translateToPigLatin = function (sentence) {
-    var pigLatinArray = sentence.split("");
+
+  var finalResult = "";
+
+  var words = sentence.split(" ");
+  for (var j = 0; j < words.length; j += 1) {
+
+    var pigLatinArray = words[j].split("");
 
     var vowels = ["a", "e", "i", "o", "u"];
 
     //
     var regex = /[^a-z]/;
-    if (sentence.match(regex)) {
-      return sentence;
+    if (words[j].match(regex)) {
+      result += sentence;
     }
 
-    else if (sentence.length === 1) { //check if sentence is one letter word
-      return (sentence + "ay"); // if above is true: execute; and leave the func
+    else if (words[j].length === 1) { //check if sentence is one letter word
+      result = (words[j] + "way"); // if above is true: execute; and leave the func
     }
     //if above is false; it goes to the next "else if"
     //we check if the first letter (pigLatinArray[0]) is a vowel
     else if ((pigLatinArray[0] === "a") || (pigLatinArray[0] === "e") || (pigLatinArray[0] === "i") || (pigLatinArray[0] === "o") || (pigLatinArray[0] === "u")) {
-      return (sentence + "way"); // if above is true: execute and leave the func
+      result = (words[j] + "way"); // if above is true: execute and leave the func
     }
     else if ((pigLatinArray[0] === "q") || (pigLatinArray[1] === "u")) {
       var result = ""
@@ -23,7 +29,6 @@ var translateToPigLatin = function (sentence) {
         result += pigLatinArray[i]
       }
       result += pigLatinArray[0] + pigLatinArray[1] + "ay"
-      return result;
     }
     else if ((pigLatinArray[1] === "q") || (pigLatinArray[2] === "u")) {
       var result = ""
@@ -31,17 +36,19 @@ var translateToPigLatin = function (sentence) {
         result += pigLatinArray[i]
       }
       result += pigLatinArray[0] + pigLatinArray[1] + pigLatinArray[2] + "ay"
-      return result;
+
     }
     // if above is false: go to next "else if"
     //we check if the SECOND letter (pigLatinArray[1]) is a vowel
-    else if ((pigLatinArray[1] === "a") || (pigLatinArray[1] === "e") || (pigLatinArray[1] === "i") || (pigLatinArray[1] === "o") || (pigLatinArray[1] === "u")){
+    else if ((pigLatinArray[1] === "a") || (pigLatinArray[1] === "e") || (pigLatinArray[1] === "i") || (pigLatinArray[1] === "o") || (pigLatinArray[1] === "u") || (pigLatinArray[1] === "y")) {
       var result = ""; // index begins at 1 which pulls the second letter and on into the variable result. Empty string in this case.
       for (var i = 1; i < pigLatinArray.length; i += 1) {
         result += pigLatinArray[i] //we loop starting from the second letter to the last; and each time add a letter to "result" string
       }
       result += pigLatinArray[0] + "ay"; // after the loop is done, we add the first letter and "ay"
-      return result; // we return result and leave the func;
+      // return result; // we return result and leave the func;
+      // finalResult += result + " ";
+
     }
 
     else if ((pigLatinArray[2] === "a") || (pigLatinArray[2] === "e") || (pigLatinArray[2] === "i") || (pigLatinArray[2] === "o") || (pigLatinArray[2] === "u")){
@@ -50,7 +57,6 @@ var translateToPigLatin = function (sentence) {
         result += pigLatinArray[i]
       }
       result += pigLatinArray[0] + pigLatinArray[1] + "ay";
-      return result;
     }
 
     else {
@@ -59,8 +65,13 @@ var translateToPigLatin = function (sentence) {
         result += pigLatinArray[i]
       }
       result += pigLatinArray[0] + pigLatinArray[1] + pigLatinArray[2]+ "ay";
-      return result;
+
     }
+    finalResult += result + " ";
+
+  }
+
+  return finalResult;
 
 
 
