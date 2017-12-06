10d939f1402ca5a4e40ac1924cb1f47ff8a43e56
Kate Trahan and Natalia Telpukhova <natalia@>
2017-12-06 09:44:31 -0800

    add spec ; initial condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
+var translateToPigLatin = function (sentence) {
+    // var pigLatinArray = sentence.split("");
+    if (sentence !== /[^a-z]/) {
+      return sentence;
+    }
+
+}
+
+
+
+
+//Front End
 $(document).ready(function() {
   $("#pig-latin").submit(function(event) {
+    event.preventDefault();
     var sentence = $("input#sentence").val();
 
     var result = translateToPigLatin(sentence);
