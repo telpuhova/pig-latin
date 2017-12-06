e3641f581a13aa6e7c2b0f618301c89e8e0664a4
Kate Trahan and Natalia Telpukhova <natalia@>
2017-12-06 10:19:32 -0800

    add spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 var translateToPigLatin = function (sentence) {
-    // var pigLatinArray = sentence.split("");
+    var pigLatinArray = sentence.split("");
+
+    var vowels = ["a", "e", "i", "o", "u"];
 
     var regex = /[^a-z]/;
     if (sentence.match(regex)) {
       return sentence;
     } else if (sentence.length === 1) {
       return (sentence + "ay");
-    } else {
+    } else if ((pigLatinArray[0] === "a") || (pigLatinArray[0] === "e") || (pigLatinArray[0] === "i") || (pigLatinArray[0] === "o") || (pigLatinArray[0] === "u")) {
       return (sentence + "way");
+    } else {
+      var result = ""; // index begins at 1 which pulls the second letter and on into the variable result. Empty string in this case.
+      for (var i = 1; i < pigLatinArray.length; i += 1) {
+        result += pigLatinArray[i]
+      }
+      result += pigLatinArray[0] + "ay";
+      return result;
     }
 
 }
